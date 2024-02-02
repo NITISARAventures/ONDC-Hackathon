c502e9455e86485dda05bfaa84c882b6bbd981e7
Kushagra-05 <s8248708@gmail.com>
2024-02-02 18:50:00 +0530

    Marker appearance sorted by adding img in assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,11 +21,32 @@ export class AppComponent implements AfterViewInit {
   }
 
   private initMap(): void {
-    this.map = L.map('map').setView([28.553440, 77.214241], 13);
+    this.map = L.map('map').setView([26.4499, 80.3319], 5);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '© OpenStreetMap contributors'
     }).addTo(this.map);
+  
+
+   // Set the icon's URL to the correct path
+   const iconUrl = 'assets/leaflet/images/marker-icon.png';
+   const iconRetinaUrl = 'assets/leaflet/images/marker-icon-2x.png';
+   const shadowUrl = 'assets/leaflet/images/marker-shadow.png';
+   const iconDefault = L.icon({
+     iconUrl,
+     iconRetinaUrl,
+     shadowUrl,
+     iconSize: [25, 41],
+     iconAnchor: [12, 41],
+     popupAnchor: [1, -34],
+     tooltipAnchor: [16, -28],
+     shadowSize: [41, 41]
+   });
+   L.Marker.prototype.options.icon = iconDefault;
+
+   // Now, when you add a marker, it will use the correct icon
+   //L.marker([28.553440, 77.214241]).addTo(this.map);
   }
+ 
 }
